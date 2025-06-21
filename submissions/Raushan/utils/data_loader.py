61de3ccb1293d82b0cac10a872a1f0988a97c0e2
RaushanBhanu <roushan.bhanu123@gmail.com>
2025-06-21 18:31:16 +0530

    fix: utils/data_loader.py

--- submissions/Raushan/utils/data_loader.py
+++ submissions/Raushan/utils/data_loader.py
@@ -1,7 +1,9 @@
-# utils/data_loader.py
+import os
 import pandas as pd
 import streamlit as st
 
 @st.cache_data
-def load_data(path: str) -> pd.DataFrame:
-    return pd.read_csv(path)
+def load_data(filename: str):
+    base_path = os.path.dirname(__file__)
+    full_path = os.path.join(base_path, "..", filename)
+    return pd.read_csv(full_path)
